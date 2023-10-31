432e55d7cd98003fae8c88032f478ad562a55a55
implodingduck <zsek320@gmail.com>
2023-10-31 13:40:04 -0500

    testing

--- recipes/redis/outputs.tf
+++ recipes/redis/outputs.tf
@@ -1,8 +1,8 @@
 output "result" {
   value = {
     values = {
-      host = "${kubernetes_service.metadata.name}.${kubernetes_service.metadata.namespace}.svc.cluster.local"
-      port = kubernetes_service.spec.port[0].port
+      host = "${kubernetes_service.redis.metadata.0.name}.${kubernetes_service.redis.metadata.0.namespace}.svc.cluster.local"
+      port = kubernetes_service.redis.spec.port[0].port
       username = ""
     }
     secrets = {
@@ -10,8 +10,8 @@ output "result" {
     }
     // UCP resource IDs
     resources = [
-        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata.namespace}/providers/core/Service/${kubernetes_service.redis.metadata.name}",
-        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.redis.metadata.name}"
+        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata.0.namespace}/providers/core/Service/${kubernetes_service.redis.metadata.0.name}",
+        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata.0.namespace}/providers/apps/Deployment/${kubernetes_deployment.redis.metadata.0.name}"
     ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
