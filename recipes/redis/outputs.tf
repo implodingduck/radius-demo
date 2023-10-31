29f232b404dc2334c52f75e72293a31a0c28bc7d
implodingduck <zsek320@gmail.com>
2023-10-31 14:04:39 -0500

    testing

--- recipes/redis/outputs.tf
+++ recipes/redis/outputs.tf
@@ -5,6 +5,34 @@ output "result" {
       port = kubernetes_service.redis.spec.0.port.0.port
       username = ""
     }
+    values = {
+      host = "${kubernetes_service.redis.metadata.0.name}.${kubernetes_service.redis.metadata.0.namespace}.svc.cluster.local"
+      port = kubernetes_service.redis.spec.0.port.0.port
+      username = ""
+    }
+    secrets = {
+      password = ""
+    }
+    // UCP resource IDs
+    resources = [
+        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata.0.namespace}/providers/core/Service/${kubernetes_service.redis.metadata.0.name}",
+        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata.0.namespace}/providers/apps/Deployment/${kubernetes_deployment.redis.metadata.0.name}"
+    ]
+  }
+  description = "The result of the Recipe. Must match the target resource's schema."
+  sensitive = true
+}
+output "properties" {
+  value = {
+    host = "${kubernetes_service.redis.metadata.0.name}.${kubernetes_service.redis.metadata.0.namespace}.svc.cluster.local"
+    port = kubernetes_service.redis.spec.0.port.0.port
+    username = ""
+    
+    values = {
+      host = "${kubernetes_service.redis.metadata.0.name}.${kubernetes_service.redis.metadata.0.namespace}.svc.cluster.local"
+      port = kubernetes_service.redis.spec.0.port.0.port
+      username = ""
+    }
     secrets = {
       password = ""
     }
