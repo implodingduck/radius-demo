f1f67b4dcfdf56ff03703a8fb0c3f2b18640be84
implodingduck <zsek320@gmail.com>
2023-10-31 14:23:08 -0500

    testing

--- recipes/redis/main.tf
+++ recipes/redis/main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
 
 resource "kubernetes_deployment" "redis" {
   metadata {
