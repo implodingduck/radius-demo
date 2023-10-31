406fb002c9c725982837098ee4f68bea1f12aa95
implodingduck <zsek320@gmail.com>
2023-10-31 13:38:38 -0500

    testing

--- recipes/redis/outputs.tf
+++ recipes/redis/outputs.tf
@@ -10,8 +10,8 @@ output "result" {
     }
     // UCP resource IDs
     resources = [
-        "/planes/kubernetes/local/namespaces/${kubernetes_service.metadata.namespace}/providers/core/Service/${kubernetes_service.metadata.name}",
-        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.metadata.name}"
+        "/planes/kubernetes/local/namespaces/${kubernetes_service.redis.metadata.namespace}/providers/core/Service/${kubernetes_service.redis.metadata.name}",
+        "/planes/kubernetes/local/namespaces/${kubernetes_deployment.redis.metadata.namespace}/providers/apps/Deployment/${kubernetes_deployment.redis.metadata.name}"
     ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
